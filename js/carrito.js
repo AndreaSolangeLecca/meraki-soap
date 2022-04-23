3c2561da9e2588a6b0704f84b7c8339bb56d27c2
Andrea Lecca <leccasolange@gmail.com>
2022-04-23 19:05:31 -0300

    añadí al icono carrito el link de carrito.html, añadi al index una simulació de incio guardandolo con json, modifiqué el archivo js de carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,65 +1,56 @@
-const PRODUCTOS = [];
+const Clickbutton = document.querySelectorAll('.btnAgregarAlCarrito');
+const tbody = document.querySelector('.tbody');
+let carrito = []
+Clickbutton.forEach(btn => {
+    btn.addEventListener('click', addToCarritoItem)
+});
 
-document.getElementById("formulario").addEventListener("submit", (e) => {
-    e.preventDefault();
+//declaro una funcion para obtener la información del boton al que hice click//
+function addToCarritoItem(e) {
 
-    let producto = document.getElementById("producto").value;
-    let precio = document.getElementById("precio").value;
+    const button = e.target
+    const item = button.closest('.productos_section_ul_li')
+    const itemTitulo = item.querySelector('.tituloProducto').textContent;
+    const itemPrecio = item.querySelector('.precioProducto').textContent;
+    const itemImg = item.querySelector('.productos_section_ul_li_img').src;
 
-    if (producto == "" || precio == "") {
-        alert("Se deben completar todos los campos");
-    } else {
-        alert("Calculo exitoso!");
-
-        const informacionDelProducto = {
-            producto,
-            precio,
-        };
-        PRODUCTOS.push(informacionDelProducto);
-        console.log(PRODUCTOS);
-
-        crearProductosEnElHTML();
-        document.getElementById("formulario").reset();
+    //creo un nuevo objeto//
+    const newItem = {
+        titulo: itemTitulo,
+        precio: itemPrecio,
+        img: itemImg,
+        cantidad: 1
     }
-})
 
-function crearProductosEnElHTML() {
-    let contenedor = document.getElementById("tbody");
-    contenedor.innerHTML = "";
-    for (let i = 0; i < PRODUCTOS.length; i++) {
-        let producto = PRODUCTOS[i].producto;
-        console.log(producto);
-        let precio = PRODUCTOS[i].precio;
-        console.log(precio);
+    addItemCarrito(newItem)
+}
 
-        contenedor.innerHTML +=
-         `
-        <tr id="${i}">
-            <td>${producto}</td>
-            <td>${precio}</td>
-            <td><button class="botones_meraki" onClick="editarProductosSinRepetir(${i})">Editar</button></td>
-            <td><button class="botones_meraki" onClick="eliminarProducto(${i})">Vaciar</button></td>
-        </tr>
+
+//con esta función le agrego a la variable carrito el objeto nuevo //
+function addItemCarrito(newItem) {
+        carrito.push(newItem)
+        renderCarrito()
+    }
+
+    function renderCarrito() {
+        tbody.innerHTML = '';
+        carrito.map(item => {
+            const tr = document.createElement('tr')
+            tr.classList.add('ItemCarrito')
+            const Content =
+                `
+           <td class="tabla_imgProducto">
+               <img src=${item.img} alt="Jabón limón y cúrcuma">
+               <h5>${item.titulo}</h5>
+           </td>
+           <td class="tabla_precioProducto">${item.precio}
+           </td>
+           <td><input type="number" min="1" value=${item.cantidad} class="input__elemento">
+               <button class="botones_meraki botonesCarrito">Vaciar</button>
+           </td>
         `
+            tr.innerHTML = Content;
+            tbody.append(tr);
+
+        })
     }
-}
-function editarProductosSinRepetir(i) {
-    let contenedor = document.getElementById(`${i}`);
-    contenedor.innerHTML = "";
-
-    let producto = PRODUCTOS[i].producto;
-    let precio = PRODUCTOS[i].precio;
-
-    document.getElementById("producto").value = producto;
-    document.getElementById("precio").value = precio;
-
-    PRODUCTOS.splice(i,1);
-}
-
-function eliminarProducto(i){
-    let contenedor = document.getElementById(`${i}`);
-    contenedor.innerHTML = "";
-
-    PRODUCTOS.splice(i,1);
-    crearProductosEnElHTML();
-}
--- js/codigoindex.js
+++ js/codigoindex.js
@@ -1,33 +1,8 @@
-let nombreIngresado = prompt("Ingrese su nombre");
-let apellidoIngresado = prompt("Ingrese su apellido");
-if ((nombreIngresado != "") && (apellidoIngresado != "")) {
-  alert("¡Hola " + nombreIngresado + " " + apellidoIngresado+"!");
+let usuario;
+let usuarioEnLS = JSON.parse(localStorage.getItem("usuario"));
+
+if (usuarioEnLS) {
+    usuario = usuarioEnLS
 } else {
-  alert("Error: Ingresar nombre y apellido");
+    usuario = prompt("Ingrese su nombre de usuario")
 }
-
-let nombreUsuario = prompt("Ingrese su usuario:"); //andrea
-let contrasenaIngresada = prompt("Ingrese su contraseña:"); //1111
-
-alert(`${nombreIngresado} Corrobore los datos por favor.`)
-
-let usuario = '';
-let pass = '';
-let intentos = 3;
-
-function login(nombreUsuario, contrasenia, intentos) {
-  if (!(nombreUsuario === '' && contrasenia === '')) {
-    while (intentos > 0 && (intentos != 0 && nombreUsuario !== usuario || contrasenia !== pass)) {
-
-      usuario = prompt("Ingrese su usuario:"); //andrea
-      pass = prompt("Ingrese su contraseña:"); //1111
-      
-      if(usuario === nombreUsuario && pass === contrasenia) {
-        alert("¡Bienvenid@! Acceso permitido");
-      }else {
-        alert(`Usuario o contraseña incorrectos. Por favor vuelva a intentarlo, te quedan ${intentos--} intentos.`);
-      }
-    }
-  }
-}
-login(nombreUsuario, contrasenaIngresada, intentos)
